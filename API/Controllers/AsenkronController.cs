a5a39886fddb5781693c84b4b26f1e18d39d006a
Sinan <sinan_26@msn.com>
2020-01-30 18:25:52 +0300

    Asenkron denemesi yiçin kodlar tamamlandı

---
+++ API/Controllers/AsenkronController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AsenkronController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Random random;
+        public AsenkronController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            random = new Random();
+
+        }
+
+        [HttpGet]
+        public async Task<int> GetAsync()
+        {
+            var delay = random.Next(1000,5000);
+            await Task.Delay(delay).ConfigureAwait(false);
+            return delay;
+        }
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var numbers = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                numbers.Add(i);
+            }
+            // await AsyncRequestAsync(i).ConfigureAwait(false);
+            var tasks = numbers.Select(n => AsyncRequestAsync(n)).ToList();
+            await Task.WhenAll(tasks);
+            Console.ReadKey();
         }
+
+        private static async Task AsyncRequestAsync(int requestId)
+        {
+            string baseUrl = "http://localhost:5000/Asenkron";
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage res = await client.GetAsync(baseUrl).ConfigureAwait(false))
+            using (HttpContent content = res.Content)
+            {
+                string data = await content.ReadAsStringAsync().ConfigureAwait(false);
+                if (data != null)
+                {
+                    Console.WriteLine("Request Id: " + requestId + "  --> Delay time:" + data);
+                }
+            }
+        }
+
     }
 }
